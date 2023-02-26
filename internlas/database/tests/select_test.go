d82aa6eb7e72fe4701bf71c0935ba2793701d39a
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-02-26 20:18:57 +0100

    completed tests

--- internlas/database/tests/select_test.go
+++ internlas/database/tests/select_test.go
@@ -4,94 +4,8 @@ import (
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/lucastomic/syn-auth/internlas/comparator"
 )
-
-// TODO:Terminar
-
-// Set of tests for InsertInto function which should return no error
-var selectTestsRight = []struct {
-	table string
-	body  map[string]any
-}{
-	{
-		"product",
-		map[string]any{
-			"name":         "Merluza",
-			"arrivalDate":  "2023-02-17",
-			"arrivalPlace": "Barcelona",
-		},
-	},
-	{
-		"user",
-		map[string]any{
-			"name":      "Lucas Tomic",
-			"nif":       "12345678Z",
-			"phone":     "623029222",
-			"ubication": "Madrid, Spain",
-		},
-	},
-	{
-		"caliber",
-		map[string]any{
-			"name":    "1-3kg",
-			"amount":  25,
-			"price":   10.5,
-			"weight":  5.6,
-			"product": "Merluza",
-		},
-	},
-	{
-		"sale",
-		map[string]any{
-			"date":          "2022-11-18",
-			"payedAtMoment": true,
-			"amount":        12,
-			"userID":        1,
-			"product":       "Merluza",
-			"caliberName":   "1-3kg",
-		},
-	},
-}
-
-// Set of tests for InsertInto function which should return error
-var selectTestsWrong = []struct {
-	table string
-	body  map[string]any
-}{
-	{
-		"product",
-		map[string]any{
-			"arrivalDate":  "2023-02-17",
-			"arrivalPlace": "Barcelona",
-		},
-	},
-	{
-		"user",
-		map[string]any{
-			"nif":       "12345678Z",
-			"phone":     "623029222",
-			"ubication": "Madrid, Spain",
-		},
-	},
-	{
-		"caliber",
-		map[string]any{
-			"name":    "1-3kg",
-			"amount":  "A lot",
-			"price":   10.5,
-			"weight":  5.6,
-			"product": "Merluza",
-		},
-	},
-	{
-		"sale",
-		map[string]any{
-			"payedAtMoment": "yes",
-			"amount":        12,
-			"caliberName":   "1-3kg",
-		},
-	},
-}
 
 // Set of rows to insert for test the select function
 var insertRowsForSelectTesting = []struct {
@@ -126,9 +40,19 @@ var insertRowsForSelectTesting = []struct {
 		},
 	},
 	{
+		"caliber",
+		map[string]any{
+			"name":    "4-6kg",
+			"amount":  12,
+			"price":   14.5,
+			"weight":  8.0,
+			"product": "Merluza",
+		},
+	},
+	{
 		"sale",
 		map[string]any{
-			"date":          "2022-11-18",
+			"date":          "2022-11-18 00:00:00",
 			"payedAtMoment": true,
 			"amount":        12,
 			"userID":        1,
@@ -138,29 +62,72 @@ var insertRowsForSelectTesting = []struct {
 	},
 }
 
+// Set of tests for InsertInto function which should return no error
+var selectTestsRight = []struct {
+	table    string
+	columns  []string
+	expected []map[string]any
+}{
+	{
+		table:   "product",
+		columns: []string{"name", "arrivalDate"},
+		expected: []map[string]any{
+			{
+				"name":        "Merluza",
+				"arrivalDate": "2023-02-17 00:00:00",
+			},
+		},
+	},
+	{
+		table:   "user",
+		columns: []string{"*"},
+		expected: []map[string]any{
+			{
+				"id":        1,
+				"business":  nil,
+				"name":      "Lucas Tomic",
+				"nif":       "12345678Z",
+				"phone":     "623029222",
+				"ubication": "Madrid, Spain",
+			},
+		},
+	},
+	{
+		table:   "caliber",
+		columns: []string{"name", "amount"},
+		expected: []map[string]any{
+			{
+				"name":   "1-3kg",
+				"amount": 25,
+			},
+			{
+				"name":   "4-6kg",
+				"amount": 12,
+			},
+		},
+	},
+}
+
 func TestSelect(t *testing.T) {
 	truncateAllTables()
-
 	db, err := connectTestingDB()
 	if err != nil {
 		t.Errorf("Error connecting to the database: %v", err)
 		return
 	}
-	for _, tt := range insertIntoTestsRight {
-		t.Run(tt.table, func(t *testing.T) {
-			// t.Parallel()
-			err := db.InsertInto(tt.table, tt.body)
-			if err != nil {
-				t.Errorf("Error inserting the values in the database: %v", err)
-			}
-		})
+	for _, tt := range insertRowsForSelectTesting {
+		db.InsertInto(tt.table, tt.body)
 	}
-	for _, tt := range insertIntoTestsWrong {
+
+	for _, tt := range selectTestsRight {
 		t.Run(tt.table, func(t *testing.T) {
 			// t.Parallel()
-			err := db.InsertInto(tt.table, tt.body)
-			if err == nil {
-				t.Error("Should return error")
+			res, err := db.Select(tt.table, tt.columns)
+			if err != nil {
+				t.Errorf("Error selecting the values from the database: %v", err)
+			}
+			if !comparator.CompareMapSlices(res, tt.expected) {
+				t.Errorf("Incorrect value retrived. Expected: %v, Got: %v", tt.expected, res)
 			}
 		})
 	}
