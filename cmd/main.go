58c3577c588868d176469418454273e7efd02ead
Lucas Tomic Benitez <ltomicb@gmail.com>
2023-02-28 19:10:45 +0100

    Add log when server starts

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	requesthandler "github.com/lucastomic/syn-auth/internlas/request_handler"
 )
 
 func main() {
+	fmt.Println("API server raised. Listening at port 8080")
 	requesthandler.ListenAndServe()
 }
